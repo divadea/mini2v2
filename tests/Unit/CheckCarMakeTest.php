3f443e7628aba36163279771a02f9b7ab4edfc01
Deanna <bridge.dea@gmail.com>
2019-04-15 23:08:15 +0000

    14. Create a unit test to check if a car's make is either ford / honda / toyota

---
+++ tests/Unit/CheckCarMakeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Car;
+
+class CheckCarMakeTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+  
+    public function testExample()
+    {
+        $makeArray = ['ford','honda' ,'toyota'];
+        #factory(Car::class)->create();
+        $car =  Car::first();
+        $this->assertContains( $car->make, $makeArray );
+        $this->assertTrue(true);
+    }
+}
